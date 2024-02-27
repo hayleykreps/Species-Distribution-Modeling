84c071ba97c595060c2e9005660d42b5408b1d1b
hayleykreps <lc21-0968@lclark.edu>
2024-02-27 19:51:03 +0000

    Created file for map and installed packages

--- src/gbif.R
+++ src/gbif.R
@@ -56,5 +56,6 @@ fData<-fData %>%
 #cc_sea(lon="decimalLongitude", lat="decimalLatitude")
 #distinct(decimalLongitude, decimalLatitude, speciesKey, datasetKey, .keep_all=TRUE)
 
+write.csv(fData, "data/cleanedData.csv")
 
 
